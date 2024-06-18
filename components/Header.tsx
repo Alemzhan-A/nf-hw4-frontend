4193a8e5f8ee87925e14f4f1ee97bfad5a5395d4
Alemzhan-A <alimzanalimzag@gmail.com>
2024-06-18 10:45:38 +0500

    fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
@@ -8,21 +9,27 @@ const Header: React.FC = () => {
         <nav>
           <ul className="flex space-x-4">
             <li className="relative group">
-              <a href="/" className="px-3 py-2 block rounded-md transition-colors duration-300 hover:bg-coral-dark">
-                Home
-              </a>
+              <Link href="/" passHref>
+                <a className="px-3 py-2 block rounded-md transition-colors duration-300 hover:bg-coral-dark">
+                  Home
+                </a>
+              </Link>
               <span className="absolute inset-x-0 bottom-0 h-1 bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
             </li>
             <li className="relative group">
-              <a href="/" className="px-3 py-2 block rounded-md transition-colors duration-300 hover:bg-coral-dark">
-                Products
-              </a>
+              <Link href="/products" passHref>
+                <a className="px-3 py-2 block rounded-md transition-colors duration-300 hover:bg-coral-dark">
+                  Products
+                </a>
+              </Link>
               <span className="absolute inset-x-0 bottom-0 h-1 bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
             </li>
             <li className="relative group">
-              <a href="/create-product" className="px-3 py-2 block rounded-md transition-colors duration-300 hover:bg-coral-dark">
-                Add Product
-              </a>
+              <Link href="/create-product" passHref>
+                <a className="px-3 py-2 block rounded-md transition-colors duration-300 hover:bg-coral-dark">
+                  Add Product
+                </a>
+              </Link>
               <span className="absolute inset-x-0 bottom-0 h-1 bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
             </li>
           </ul>
